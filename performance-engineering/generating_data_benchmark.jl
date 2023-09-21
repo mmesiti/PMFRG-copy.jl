c342e18243f1dd16e55a65856d9b119a7ac39ae8
Michele Mesiti <michele.mesiti@kit.edu>
2023-09-21 14:24:24 +0200

    Fixed generating script, found bug - to correct
    When the expression defining the function is declared at the top scope
    and it is redeclared, the wrong version might be picked up.

--- performance-engineering/generating_data_benchmark.jl
+++ performance-engineering/generating_data_benchmark.jl
@@ -23,7 +23,74 @@ couplings = [J1, J2]
 SystemToy = getSquareLattice(NLenToy, couplings)
 @time "generate" compute_intensive_toy_expr = PMFRG.generate_compute_intensive(SystemToy)
 @time "eval" compute_intensive_toy = eval(compute_intensive_toy_expr)
+function trigger_compile_compute_intensive_toy()
+    Npairs = SystemToy.Npairs
+    NUnique = SystemToy.NUnique
+    a = zeros(Float64, (Npairs, 1, 1, 1))# ::Array{T,4},
+    b = zeros(Float64, (Npairs, 1, 1, 1))# ::Array{T,4},
+    c = zeros(Float64, (Npairs, 1, 1, 1))# ::Array{T,4},
+    Va12 = zeros(Float64, (Npairs,)) # ::Vector{T},
+    Vb12 = zeros(Float64, (Npairs,)) # ::Vector{T},
+    Vc12 = zeros(Float64, (Npairs,)) # ::Vector{T},
+    Va34 = zeros(Float64, (Npairs,)) # ::Vector{T},
+    Vb34 = zeros(Float64, (Npairs,)) # ::Vector{T},
+    Vc34 = zeros(Float64, (Npairs,)) # ::Vector{T},
+    Vc21 = zeros(Float64, (Npairs,)) # ::Vector{T},
+    Vc43 = zeros(Float64, (Npairs,)) # ::Vector{T},
+    Props = SMatrix{NUnique,NUnique,Float64}(zeros(Float64, (NUnique, NUnique)))
+    is = 1 #::Integer,
+    it = 1 #::Integer,
+    iu = 1 #::Integer
+    compute_intensive_toy(a,
+        b,
+        c,
+        Va12,
+        Vb12,
+        Vc12,
+        Va34,
+        Vb34,
+        Vc34,
+        Vc21,
+        Vc43,
+        Props,
+        is,
+        it,
+        iu)
+end
 
+@time "compilation" trigger_compile_compute_intensive_toy()
+
+println("Warm up")
+
+Par = Params( #create a group of all parameters to pass them to the FRG Solver
+    SystemToy, # geometry, this is always required
+    OneLoop(), # method. OneLoop() is the default
+    T=0.5, # Temperature for the simulation.
+    N=10, # Number of positive Matsubara frequencies for the four-point vertex.
+    accuracy=1e-3, #absolute and relative tolerance of the ODE solver.
+    # For further optional arguments, see documentation of 'NumericalParams'
+    MinimalOutput=true,
+)
+
+tempdir = "temp"
+println("Removing data from previous runs ($tempdir)")
+rm(tempdir, recursive=true, force=true)
+mainFile = "$tempdir/" * PMFRG.generateFileName(Par, "_testFile") # specify a file name for main Output
+flowpath = "$tempdir/flows/" # specify path for vertex checkpoints
+
+Solution, saved_values = SolveFRG(
+    compute_intensive_toy,
+    Par,
+    MainFile=mainFile,
+    CheckpointDirectory=flowpath,
+    method=DP5(),
+    VertexCheckpoints=[],
+    CheckPointSteps=3,
+);
+
+
+
+println("Warmup done, timing real problem now.")
 System = getSquareLattice(NLen, couplings)
 @time "generate" compute_intensive_expr = PMFRG.generate_compute_intensive(System)
 @time "eval" compute_intensive = eval(compute_intensive_expr)
@@ -65,37 +132,6 @@ end
 @time "compilation" trigger_compile_compute_intensive()
 
 
-println("Warm up")
-
-Par = Params( #create a group of all parameters to pass them to the FRG Solver
-    SystemToy, # geometry, this is always required
-    OneLoop(), # method. OneLoop() is the default
-    T=0.5, # Temperature for the simulation.
-    N=10, # Number of positive Matsubara frequencies for the four-point vertex.
-    accuracy=1e-3, #absolute and relative tolerance of the ODE solver.
-    # For further optional arguments, see documentation of 'NumericalParams'
-    MinimalOutput=true,
-)
-
-tempdir = "temp"
-println("Removing data from previous runs ($tempdir)")
-rm(tempdir, recursive=true, force=true)
-mainFile = "$tempdir/" * PMFRG.generateFileName(Par, "_testFile") # specify a file name for main Output
-flowpath = "$tempdir/flows/" # specify path for vertex checkpoints
-
-Solution, saved_values = SolveFRG(
-    compute_intensive_toy,
-    Par,
-    MainFile=mainFile,
-    CheckpointDirectory=flowpath,
-    method=DP5(),
-    VertexCheckpoints=[],
-    CheckPointSteps=3,
-);
-
-
-
-println("Warmup done, timing real problem now.")
 
 
 Par = Params( #create a group of all parameters to pass them to the FRG Solver
